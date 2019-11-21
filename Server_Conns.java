4c765f4bf0ca165103606719880d400b2d09469e
gg-cyber172 <55152466+gg-cyber172@users.noreply.github.com>
2019-11-21 16:25:14 +0000

    Update Server_Conns.java

--- Server_Conns.java
+++ Server_Conns.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public interface Server_Conns{ //Global list of all the connections
        public ArrayList<Socket> conn_list= new ArrayList<Socket>();//[5];
 }
