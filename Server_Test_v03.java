935dff7ad735c4278240e7c0f5c32e87f40b62de
gg-cyber172 <55152466+gg-cyber172@users.noreply.github.com>
2019-11-29 12:17:27 +0000

    Updated so the Socket runs on 127.0.0.1

--- Server_Test_v03.java
+++ Server_Test_v03.java
@@ -8,7 +8,7 @@ public class Server_Test_v03 implements Server_Conns{
         String input="";
         String server_input="";
         InetAddress ip = InetAddress.getByName("127.0.0.1");
-        try (ServerSocket servSock = new ServerSocket(port)){
+        try (ServerSocket servSock = new ServerSocket(port,ip)){
             System.out.println("Server is on " +ip+ ":"+port);
             while (true){//(!((server_input=System.console().readLine()).equals("exit")))  {
                 Socket socket = servSock.accept();
